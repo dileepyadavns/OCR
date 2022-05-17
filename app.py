5811c01bb79db167e8b8d6d6dceea373b4b2ce28
dileepyadavns <dileepyadavneo@gmail.com>
2022-05-17 20:44:46 +0530

    applied css styling and tried to improve accuracy of tesseract  by basic image processing

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
+#Importing libraries
 import torch
 import io
 from PIL import Image
 import cv2
-import argparse
-import imutils
 import numpy as np
-
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from itsdangerous import Signer, BadSignature, want_bytes
 import psycopg2 #pip install psycopg2 
 import psycopg2.extras
-import pytesseract
+import pytesseract #for number plate extraction
+
 conn = psycopg2.connect( #psycopg2 database adaptor for implementing python
         host="localhost",
         database="students",
@@ -20,20 +19,22 @@ conn = psycopg2.connect( #psycopg2 database adaptor for implementing python
 
 app = Flask(__name__)
 
+#Loading the yolov5 model with the trained weights of  custom dataset
 model = torch.hub.load( '/home/neosoft/Documents/OCR/yolov5','custom',path='/home/neosoft/Documents/OCR/best.pt',source='local') # for PIL/cv2/np inputs and
 
+#to detect the number plate
 def get_prediction(img_bytes):
     img = Image.open(io.BytesIO(img_bytes))
     results = model(img, size=640)  
     return results
 
-
+#home page of flask 
 @app.route('/', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
-        file = request.files.get('file')
+        file = request.files.get('file') #to take image from user
         if not file:
             return
 
@@ -42,40 +43,14 @@ def predict():
         print(type(results))
         results.save('results0.jpg')
     
-        crops = results.crop(save=True)  
-        
-        crops=crops[0]['im']
-        #ad
-        img = cv2.cvtColor(crops, cv2.COLOR_BGR2GRAY)
-        # img = cv2.threshold(img, 0, 255,
-	    # cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        
-#         cnts = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL,
-#     	cv2.CHAIN_APPROX_SIMPLE)
-#         cnts = imutils.grab_contours(cnts)
-#         chars = []
-# # loop over the contours
-#         for c in cnts:
-#              (x, y, w, h) = cv2.boundingRect(c)
-#              if w >= 35 and h >= 100:
-#                  chars.append(c)
-
-#         chars = np.vstack([chars[i] for i in range(0, len(chars))])
-#         hull = cv2.convexHull(chars)
-#         # allocate memory for the convex hull mask, draw the convex hull on
-#         # the image, and then enlarge it via a dilation
-#         mask = np.zeros(crops.shape[:2], dtype="uint8")
-#         cv2.drawContours(mask, [hull], -1, 255, -1)
-#         mask = cv2.dilate(mask, None, iterations=2)
-#         cv2.imshow("Mask", mask)
-# # take the bitwise of the opening image and the mask to reveal *just*
-# # the characters in the image
-#         img = cv2.bitwise_and(img, img, mask=mask )       
-        #ad
-        img=Image.fromarray(img)
-        img.show()
-        text = pytesseract.image_to_string(img)
-        return render_template('results.html',path=text)   
+        crops = results.crop(save=True) #cropping the image on bounding box
+        crops=crops[0]['im'] #to get the image array from the array created from above step
+       
+        img = cv2.cvtColor(crops, cv2.COLOR_BGR2GRAY) #to convert image to gray
+        img=Image.fromarray(img) #to get image from array
+        img.show()#to display image
+        text = pytesseract.image_to_string(img)#to get the charcters from number plate
+        return render_template('results.html',path=text)#to redirect to results html page   
 
     
     return render_template('index.html')    
